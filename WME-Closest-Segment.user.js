cb5ffdccefeb181c3ad1e0aafc9af8c7144599fe
Ian "The_Cre8r" Harshbarger <TheCre8r@users.noreply.github.com>
2016-11-16 06:53:24 -0500

    Greasyfork Import 2015-03-21 04:17:52 UTC

--- WME-Closest-Segment.user.js
+++ WME-Closest-Segment.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         WME Closest Segment
-// @description  Shows the closest segment to a place
-// @version      0.8
-// @author       SAR85
-// @copyright	 SAR85
-// @license		 CC BY-NC-ND
-// @grant		 none
-// @include      https://www.waze.com/editor/*
-// @include      https://www.waze.com/*/editor/*
-// @include      https://editor-beta.waze.com/*
-// @namespace https://greasyfork.org/users/9321
+// @name         	WME Closest Segment
+// @description		Shows the closest segment to a place
+// @version      	0.8
+// @author			SAR85
+// @copyright		SAR85
+// @license		 	CC BY-NC-ND
+// @grant		 	none
+// @include			https://www.waze.com/editor/*
+// @include			https://www.waze.com/*/editor/*
+// @include			https://editor-beta.waze.com/*
+// @namespace		https://greasyfork.org/users/9321
 // ==/UserScript==
 
 (function () {
@@ -46,34 +46,39 @@
 		minDistance = Infinity,
 		distanceToSegment,
 		segments,
-		selectedItem = W.selectionManager.hasSelectedItems() ? W.selectionManager.selectedItems[0] : void 0,
+		selectedItem,
 		closestSegment = {};
 
-		if ('undefined' !== selectedItem && 'venue' === selectedItem.model.type) {
-			closestSegment.featureStop = selectedItem.model.isPoint() ? selectedItem.model.geometry : W.geometryEditing.activeEditor.navigationPoint.lonlat.toPoint();
-			segments = W.model.segments.objects;
-			for (i in segments) {
-				s = W.model.segments.get(i);
-				if (inMapExtent(s)) {
-					if (selectedItem.model.isPoint()) {
-						distanceToSegment = selectedItem.geometry.distanceTo(s.geometry, {
-								details : true
-							});
-					} else {
-						distanceToSegment = W.geometryEditing.activeEditor.navigationPoint.lonlat.toPoint().distanceTo(s.geometry, {
-								details : true
-							});
-					}
-					if (distanceToSegment.distance < minDistance) {
-						minDistance = distanceToSegment.distance;
-						closestSegment.seg = i;
-						closestSegment.point = new OL.Geometry.Point(distanceToSegment.x1, distanceToSegment.y1);
-					}
+		/* Zoom must be at least 4 in order to see streets */
+		if (W.map.getZoom() < 4) {
+			console.log('WME CS: Zoom too low.');
+			return;
+		}
+		
+		selectedItem = W.selectionManager.selectedItems.first();
+		closestSegment.featureStop = selectedItem.model.isPoint() ? selectedItem.model.geometry : W.geometryEditing.activeEditor.navigationPoint.lonlat.toPoint();
+		segments = W.model.segments.objects;
+		for (i in segments) {
+			s = W.model.segments.get(i);
+			if (inMapExtent(s)) {
+				if (selectedItem.model.isPoint()) {
+					distanceToSegment = selectedItem.geometry.distanceTo(s.geometry, {
+							details : true
+						});
+				} else {
+					distanceToSegment = W.geometryEditing.activeEditor.navigationPoint.lonlat.toPoint().distanceTo(s.geometry, {
+							details : true
+						});
+				}
+				if (distanceToSegment.distance < minDistance) {
+					minDistance = distanceToSegment.distance;
+					closestSegment.seg = i;
+					closestSegment.point = new OL.Geometry.Point(distanceToSegment.x1, distanceToSegment.y1);
 				}
 			}
-			console.log(minDistance, closestSegment);
-			drawLine(closestSegment.featureStop, closestSegment.point);
 		}
+		/* console.log(minDistance, closestSegment); */
+		drawLine(closestSegment.featureStop, closestSegment.point);
 	}
 
 	function checkSelection() {
@@ -81,28 +86,24 @@
 		var selectedItem,
 		navPoint;
 
-		console.log("Selection change called.");
+		//console.log("Selection change called.");
 
 		if (W.selectionManager.hasSelectedItems()) {
 			selectedItem = W.selectionManager.selectedItems[0];
 			if ('venue' !== selectedItem.model.type) {
-				console.log('Selection is not a place.');
+				//console.log('Selection is not a place.');
 				return;
 			}
 		} else {
-			console.log('No item selected.');
+			//console.log('No item selected.');
 			return clearLayerFeatures();
 		}
 
 		if (selectedItem.model.isPoint()) {
-			console.log('Selection is point venue.');
+			//console.log('Selection is point venue.');
 			findNearestSegment();
-			W.geometryEditing.activeEditor.dragControl.onDrag = function (e, t) {
-				W.geometryEditing.activeEditor.dragVertex.apply(W.geometryEditing.activeEditor, [e, t]);
-				findNearestSegment();
-			};
 		} else {
-			console.log('Selection is area venue');
+			//console.log('Selection is area venue');
 			navPoint = W.geometryEditing.activeEditor.navigationPoint;
 			if (null !== typeof navPoint && 'undefined' !== typeof navPoint) {
 				findNearestSegment();
@@ -113,15 +114,16 @@
 
 	function init() {
 		"use strict";
-		var closestVersion = "0.8",
+		var alertUpdate = true,
+		closestVersion = "0.8",
 		closestChanges = "WME Closest Segment has been updated to version " +
 			closestVersion + ".\n" +
 			"*First published version--beta" +
 			"*";
 
 		/* Check version and alert on update */
-		if ('undefined' === window.localStorage.closestVersion ||
-			closestVersion !== window.localStorage.closestVersion) {
+		if (alertUpdate && ('undefined' === window.localStorage.closestVersion ||
+				closestVersion !== window.localStorage.closestVersion)) {
 			alert(closestChanges);
 			window.localStorage.closestVersion = closestVersion;
 		}
@@ -130,8 +132,15 @@
 		W.loginManager.events.register("afterloginchanged", this, init);
 		W.model.actionManager.events.register("afterundoaction", this, clearLayerFeatures);
 		W.selectionManager.events.register("selectionchanged", this, checkSelection);
+
+		W.geometryEditing.editors.venue.dragControl.onDrag = function (e, t) {
+			W.geometryEditing.activeEditor.dragVertex.apply(W.geometryEditing.activeEditor, [e, t]);
+			findNearestSegment();
+		};
+
 		/* Add map layer */
 		W.map.addLayer(new OL.Layer.Vector("Closest Segment"));
+		console.log('WME CS: Initialized.');
 	}
 
 	function bootstrap() {
@@ -153,13 +162,15 @@
 		/* begin running the code! */
 		if (('undefined' !== typeof W.loginManager.events.register) &&
 			('undefined' !== typeof W.map)) {
+			console.log('WME CS: Initializing...');
 			window.setTimeout(init, 100);
 		} else {
+			console.log('WME CS: Bootstrap failed. Trying again...');
 			window.setTimeout(function () {
 				bootstrap();
 			}, 1000);
 		}
 	}
-
+	console.log('WME CS: Bootstrap...');
 	window.setTimeout(bootstrap, 100);
 })();
