0633445df784d92b62800608f763316579a376bb
justins83 <justins83.waze@gmail.com>
2017-04-25 13:21:13 -0400

    Support for beta changes
    Support for new RPP stop point feature

--- WME-Closest-Segment.user.js
+++ WME-Closest-Segment.user.js
@@ -3,7 +3,7 @@
 // ==UserScript==
 // @name         	WME Closest Segment
 // @description		Shows the closest segment to a place
-// @version      	1.0.2.6
+// @version      	1.0.2.7
 // @author			SAR85
 // @copyright		SAR85
 // @license		 	CC BY-NC-ND
@@ -11,7 +11,7 @@
 // @include			https://www.waze.com/editor/*
 // @include			https://www.waze.com/*/editor/*
 // @include			https://beta.waze.com/*
-// @exclude                     https://www.waze.com/user/editor*
+// @exclude         https://www.waze.com/user/editor*
 // @namespace		https://greasyfork.org/en/users/13670-the-cre8r
 // ==/UserScript==
 
@@ -117,6 +117,7 @@
             if (null !== typeof navPoint) {
                 try {
                     navPoint.events.unregister('drag', W.geometryEditing.activeEditor, findNearestSegment);
+                    console.log("unregistered!");
                 } catch (err) { }
             }
         }
@@ -151,11 +152,12 @@
 			segmentType,
 			closestSegment = {};
 
-		if (selectedItem.model.isPoint()) {
+        //Have to check both navigationPoint & _navigationPointMarker so it works in both beta & production
+		if (selectedItem.model.isPoint() && W.geometryEditing.activeEditor._navigationPointMarker == null && W.geometryEditing.activeEditor.navigationPoint == null) {
 			closestSegment.featureStop = selectedItem.model.geometry;
 			closestSegment.featureIsPoint = true;
 		} else {
-			closestSegment.featureStop = W.geometryEditing.activeEditor.navigationPoint.lonlat.toPoint();
+                closestSegment.featureStop = navPoint.lonlat.toPoint();
 			closestSegment.featureIsPoint = false;
 		}
 		for (s in segmentsInExtent) {
@@ -170,12 +172,12 @@
 				segmentType === 19) {
 				continue;
 			}
-			if (selectedItem.model.isPoint()) {
+			if (selectedItem.model.isPoint() && W.geometryEditing.activeEditor._navigationPointMarker == null && W.geometryEditing.activeEditor.navigationPoint == null) {
 				distanceToSegment = selectedItem.geometry.distanceTo(segmentsInExtent[s].geometry, {
 					details: true
 				});
 			} else {
-				distanceToSegment = W.geometryEditing.activeEditor.navigationPoint.lonlat.toPoint().distanceTo(
+				distanceToSegment = navPoint.lonlat.toPoint().distanceTo(
 					segmentsInExtent[s].geometry, {
 						details: true
 					});
@@ -196,21 +198,25 @@
 		'use strict';
 		log('Selection change called.', 2);
 
-
 		if (!checkConditions()) {
 			removeDragCallbacks();
 		} else {
 			if (W.selectionManager.hasSelectedItems()) {
 				selectedItem = W.selectionManager.selectedItems[0];
+
+                if(/beta/.test(location.href))
+                    navPoint = W.geometryEditing.activeEditor._navigationPointMarker;
+                else
+                    navPoint = W.geometryEditing.activeEditor.navigationPoint;
+
 				if ('venue' !== selectedItem.model.type) {
 					log('Selection is not a place.', 2);
 					removeDragCallbacks();
 					clearLayerFeatures();
 				} else {
-                    navPoint = W.geometryEditing.activeEditor.navigationPoint;
 					getSegmentsInExtent();
                     placeIsPoint = selectedItem.model.isPoint();
-					if (placeIsPoint) {
+					if (placeIsPoint && W.geometryEditing.activeEditor._navigationPointMarker == null && W.geometryEditing.activeEditor.navigationPoint == null) {
 						log('Selection is point venue.', 2);
 						W.geometryEditing.activeEditor.dragControl.onDrag = function (e, t) {
 							W.geometryEditing.activeEditor.dragVertex.apply(W.geometryEditing.activeEditor, [e, t]);
@@ -220,7 +226,9 @@
 					} else {
 						log('Selection is area venue.', 2);
 						if (null !== typeof navPoint) {
+                            //debugger;
 							navPoint.events.register('drag', W.geometryEditing.activeEditor, findNearestSegment);
+                            console.log("registered!");
 							if (inMapExtent(navPoint.lonlat.toPoint())) {
 								findNearestSegment();
 							} else {
